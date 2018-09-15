da323c110aa7ac3d92c36ce07a4fbf6e3b2c7a10
NickSanford4 <nicksanford4@gmail.com>
2018-09-15 16:06:20 -0700

    Add files via upload

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,65 +1,59 @@
-package arrays;
+package _00_IntroToArrays;
 
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
-
 public class _01_RobotRace {
 	//1. make a main method
-public static void main(String[] args) {
-	
-    int N_robots=8;
-	//2. create an array of 5 robots.
-	Robot[] a = new Robot[N_robots];
-	//3. use a for loop to initialize the robots.
-	int xc = 500;
-	int yc = 300;
-	int R  =200;
-	int x = 0;
-	int y = 0;
-	
-for (int i = 0; i < a.length; i++) {
-	x=xc+(int)(R*Math.cos(2*Math.PI*i/N_robots));
-	y=yc+(int)(R*Math.sin(2*Math.PI*i/N_robots));
-	System.out.println(i+" "+x+" "+y);
-	a[i]=new Robot();
-	a[i].setX(x);
-	a[i].setY(y);
-	a[i].turn(i*360/N_robots);
+	public static void main(String[] args) {
+		Random rand = new Random();
+		//2. create an array of 5 robots.
+		Robot[]rob = new Robot[1];
+		//3. use a for loop to initialize the robots.
+		for (int i = 0; i < rob.length; i++) { 
+		rob[i] = new Robot();
+		//4. make each robot start at the bottom of the screen, side by side, facing up 
+		rob[i].setX(300);
+		rob[i].setY(300);
+		rob[i].penDown();
+		}
+
+		boolean finish=false;
+		int     winner=0;
+		//while (finish==false) {
+		for(int j=0;j<360;j++) {
+			 for (int i = 0; i < rob.length; i++) {
+				    //if (rob[i].getY()>0) {
+				       int t=rand.nextInt(50);
+				       for(int k=0;k<t;k++) {
+				    	   		rob[i].move(5);
+				    	   		rob[i].turn(1);
+				    	   		//rob[i].penDown();
+				    	   		rob[i].setSpeed(10);
+				       }
+//				    else {
+				    	  //  finish=true;
+				    	  //  winner=i;
+				    	  //  break;
+				   // }
+			 }
+				
+		//}
+	 
+		
+}
+        System.out.println("winner="+winner+1);
+		//7. declare that robot the winner and throw it a party!
+    		if(rob[winner].getY() <= 0) {
+    			rob[winner].sparkle();
+    			JOptionPane.showMessageDialog(null,"winner="+(winner+1));
+    		}
+		//8. try different races with different amounts of robots.
+    	
+	    //9. make the robots race around a circular track.
+
 	}
-	//4. make each robot start at the bottom of the screen, side by side, facing up
-
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-for (int q = 0; q < 90; q++) {
-	Random rand = new Random();
-for (int i = 0; i < a.length; i++) {
-	int distance = rand.nextInt(5);
-	a[i].move(distance);
-	a[i].turn(-distance);
-	a[i].move(distance);
-	a[i].setSpeed(10);
-	if(a[i].getY()<=0) {
-		JOptionPane.	showMessageDialog(null, "Robot "+(i+1)+" won  ''We all throw it a party''");
-System.exit(0);
-
-	}
-
-	a[i].setRandomPenColor();
-	a[i].penDown();
-	
-}
-}
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-//done
-	//7. declare that robot the winner and throw it a party!
-//done
-	//8. try different races with different amounts of robots.
-//done
-	//9. make the robots race around a circular track.
 
 }
-}
-
